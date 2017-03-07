9b40adcfd47dc0f6a403e7c87b0c8d967711af17
Julian David Torregrosa <juliant@lagashcoldev.com>
2017-03-07 16:59:21 -0500

    Add new routes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require('express')
-var flights = require('./testData/flights.json');
+var automoviles = require('./testData/automoviles.json');
+var hoteles = require('./testData/hoteles.json');
 var app = express()
 
 app.get('/status', function (req, res) {
   res.send({ status: "ok" });
 })
 
-app.get('/flight', function (req, res) {
-  res.send(flights);
+app.get('/automoviles', function (req, res) {
+  res.send(automoviles);
+})
+
+app.get('/hoteles', function (req, res) {
+  res.send(automoviles);
 })
 
 app.listen(process.env.PORT || 5000)
