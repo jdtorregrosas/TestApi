f384c45eba997e85b261b7b6e77a961b87f8f8d1
Julian David Torregrosa <juliant@lagashcoldev.com>
2017-03-07 16:15:33 -0500

    Change port

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/flight', function (req, res) {
   res.send(flights);
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 })
