03de0513b8c2abb22f5428453b12faabe753b66e
Julian David Torregrosa <juliant@lagashcoldev.com>
2017-03-09 16:23:05 -0500

    Add vuelos, cruceros and vacaciones

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var automoviles = require('./testData/automoviles.json');
 var hoteles = require('./testData/hoteles.json');
+var vuelos = require('./testData/vuelos.json');
+var cruceros = require('./testData/cruceros.json');
+var vacaciones = require('./testData/vacaciones.json');
 var app = express()
 
 app.get('/status', function (req, res) {
@@ -12,7 +15,20 @@ app.get('/automoviles', function (req, res) {
 })
 
 app.get('/hoteles', function (req, res) {
-  res.send(automoviles);
+  res.send(hoteles);
 })
+
+app.get('/vuelos', function (req, res) {
+  res.send(vuelos);
+})
+
+app.get('/cruceros', function (req, res) {
+  res.send(cruceros);
+})
+
+app.get('/vacaciones', function (req, res) {
+  res.send(vacaciones);
+})
+
 
 app.listen(process.env.PORT || 5000)
